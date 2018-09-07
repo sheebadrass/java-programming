3af1250113b8a37a33ada4de378a0b612aa8a37d
sheebadrass <43059316+sheebadrass@users.noreply.github.com>
2018-09-07 12:23:59 +0530

    Add files via upload

---
+++ math calculaitons.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    double b,c,d,e,f;
+    a=2;b=2.5;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("the sum is"+c);
+    System.out.println("the difference is"+d);System.out.println("the multiplied output is"+e);
+    System.out.println("the divided output is"+f);
+  }
+}
