35a492e80ea71375e6e8991ec45f56e0252cad79
sheebadrass <43059316+sheebadrass@users.noreply.github.com>
2018-10-03 11:43:01 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class HelloWorld
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.print('HELLO WORLD');
+	}
+}
