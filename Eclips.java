6471ee448b699f378e6528a8d9977adf83e70404
sheebadrass <43059316+sheebadrass@users.noreply.github.com>
2018-10-03 11:50:22 +0530

    Create Eclips.java

---
+++ Eclips.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Eclips
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println(System.getproperty("java.runtime.version"));
+	}
+}
