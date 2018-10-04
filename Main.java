1b1efdb2b840ab4ae0afa385a2cc450231fca11f
sheebadrass <43059316+sheebadrass@users.noreply.github.com>
2018-10-04 11:44:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    int number=109;
+    int number=5;
         if(number > 0)
         {
             System.out.println(number+" is a positive number");
