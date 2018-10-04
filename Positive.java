1781de9b807ecf9c891d3630c9326b05014d00bc
sheebadrass <43059316+sheebadrass@users.noreply.github.com>
2018-10-04 11:57:22 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,17 @@
+class Positive {
+  public static void main(String[] args) {
+    int a=5;
+        if(a > 0)
+        {
+            System.out.println("positive");
+        }
+        else if(a < 0)
+        {
+            System.out.println("negative");
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+    }
+}
