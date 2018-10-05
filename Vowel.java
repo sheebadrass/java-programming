7c6f2401ab88b8dd5ad91b78c497403c100db503
sheebadrass <43059316+sheebadrass@users.noreply.github.com>
2018-10-05 12:29:36 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -2,7 +2,7 @@
 
     public static void main(String[] args) {
 
-        char ch = 'a';
+        char ch = 'o';
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
